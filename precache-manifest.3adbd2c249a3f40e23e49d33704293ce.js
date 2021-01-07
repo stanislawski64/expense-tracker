89f19b4f2046081da0c2f671c09901cca2adea89
stanislawski64 <dariusz.stanislawski64@gmail.com>
2021-01-07 20:49:09 +0100

    Updates

---
+++ precache-manifest.3adbd2c249a3f40e23e49d33704293ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2462b40d51348d65b83b16d36412d64",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "bb497ee6e23003f15780",
+    "url": "/expense-tracker/static/css/main.e3e766fa.chunk.css"
+  },
+  {
+    "revision": "09557e549440ffb8bd72",
+    "url": "/expense-tracker/static/js/2.d447a019.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker/static/js/2.d447a019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb497ee6e23003f15780",
+    "url": "/expense-tracker/static/js/main.a8131ccd.chunk.js"
+  },
+  {
+    "revision": "09a18f8f5aeb6d7f42fb",
+    "url": "/expense-tracker/static/js/runtime-main.1f6e3259.js"
+  }
+]);
